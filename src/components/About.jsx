8821288dc9c05254129b07cb562803887176b397
Dixshant2313 <dixshantcharan2313@gmail.com>
2023-07-09 21:03:49 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <React.Fragment>
+      <div
+        name="about"
+        className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      >
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+          <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+              About
+            </p>
+          </div>
+
+          <p className="text-xl mt-10 sm:mt-20 mb-8">
+            I am a passionate Full Stack Developer. I specialize in building web
+            applications using modern technologies like React, Node.js, and
+            MongoDB. With a keen eye for design and a dedication to crafting
+            high-quality code, I strive to create intuitive and visually
+            appealing user experiences. I am constantly seeking opportunities to
+            expand my skill set and stay up-to-date with the latest industry
+            trends.
+          </p>
+
+          <p className="text-xl mb-8">
+            I have worked on various projects, ranging from e-commerce platforms
+            to social networking applications. I am experienced in front-end
+            development, creating responsive layouts and implementing
+            interactive features. On the back-end, I am skilled in building
+            APIs, handling databases, and ensuring robust server-side
+            functionality. I am a strong believer in clean code, continuous
+            learning, and collaborative teamwork. Let's connect and bring your
+            ideas to life!
+          </p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import netflixClone from "../assets/portfolio/netflixClone.png";
+import chitChat from "../assets/portfolio/chitChat.jpg";
+import ecommerce from "../assets/portfolio/ecommerce.jpg";
+import todoApp from "../assets/portfolio/todoApp.jpg";
+import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: chitChat,
+      demoUrl:
+        "https://64aa9cf7803f811ddbe0bb4c--comforting-gumdrop-62a440.netlify.app/",
+      codeUrl: "https://github.com/Dixshant2313/chitchat",
+    },
+    {
+      id: 2,
+      src: netflixClone,
+      demoUrl:
+        "https://netflix-clone-blush-chi.vercel.app/?vercelToolbarCode=FISLsEs8bTIuici",
+      codeUrl: "https://github.com/Dixshant2313/netflix-clone",
+    },
+    {
+      id: 3,
+      src: ecommerce,
+      demoUrl: "https://dixshant2313.github.io/Cara-Ecommerce/",
+      codeUrl: "https://github.com/Dixshant2313/Cara-Ecommerce",
+    },
+    {
+      id: 4,
+      src: todoApp,
+      demoUrl: "https://todo-react-eight-rho.vercel.app/login",
+      codeUrl: "https://github.com/Dixshant2313/todo_react",
+    },
+    {
+      id: 5,
+      src: reactSmooth,
+      demoUrl: "",
+      codeUrl: "",
+    },
+    {
+      id: 6,
+      src: reactWeather,
+      demoUrl: "https://dixshant2313.github.io/Mausam/",
+      codeUrl: "https://github.com/Dixshant2313/Mausam",
+    },
+  ];
+
+  const redirectToDemo = (demoUrl) => {
+    if (demoUrl) {
+      window.open(demoUrl, "_blank");
+    } else {
+      console.log("Demo URL not available");
+    }
+  };
+
+  const redirectToCode = (codeUrl) => {
+    if (codeUrl) {
+      window.open(codeUrl, "_blank");
+    } else {
+      console.log("Code URL not available");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <div
+        name="portfolio"
+        className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+      >
+        <div className="max-w-screen-lg p-4 mx-auto w-full h-full flex flex-col justify-center">
+          <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+              Portfolio
+            </p>
+            <p className="py-6">Check out some of my work here</p>
+          </div>
+
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+            {portfolios.map(({ id, src, demoUrl, codeUrl }) => (
+              <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+                <img
+                  src={src}
+                  alt=""
+                  className="rounded-md duration-200 hover:scale-105"
+                />
+                <div className="flex items-center justify-center">
+                  <button
+                    className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                    onClick={() => redirectToDemo(demoUrl)}
+                  >
+                    Demo
+                  </button>
+                  <button
+                    className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                    onClick={() => redirectToCode(codeUrl)}
+                  >
+                    Code
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Portfolio;
